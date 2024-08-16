8a790b9179c1f9b57740a57576a35f105ab03739
mansur <20260249+mansurGI@users.noreply.github.com>
2024-08-16 14:38:17 +0200

    +localhost to allowed hosts in settings

--- settings/base.py
+++ settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ij*u16s(b&@flfr9*0dwj4668usy*u_mkmx%@3$oc!$8qc_e4m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
